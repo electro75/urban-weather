48f2ac6189a7448f96c250a6bf5a1221ed9c93ca
Sahil Naikdhure <sanaikdhure@gmail.com>
2023-11-22 14:52:02 +0000

    get sample data from fdb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { getDatabase, ref, onValue, Database } from '@angular/fire/database';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'urban-weather';
+
+  constructor(public fdb: Database) {
+
+  }
+
+  ngOnInit(): void {
+    console.log()
+    // import data
+
+    const db = getDatabase();
+    const starCountRef = ref(this.fdb);
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val();
+      console.log(data)
+    });
+
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'urban-assign-ii',
+    appId: '1:158596798945:web:e4b4684a60c510503cdaa0',
+    databaseURL: 'https://urban-assign-ii-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'urban-assign-ii.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAoYrDlyscZ3iFxqgvuue1xb_Q5-Euog5g',
+    authDomain: 'urban-assign-ii.firebaseapp.com',
+    messagingSenderId: '158596798945',
+    measurementId: 'G-BGH3TYL6FV',
+  },
+};
